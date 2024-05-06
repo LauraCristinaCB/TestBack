6d03cbefde4c6c8666a72d5d15081163029ced2a
Laura Cespedes <ingcristinacespedes@gmail.com>
2024-05-05 23:09:32 -0500

    enable cors

--- src/controller/stateController.js
+++ src/controller/stateController.js
@@ -1,6 +1,7 @@
 const stateService = require ('../services/stateService')
 
 const getState = async function(req, res) {
+        console.log(req.query)
         const state = await stateService.getState(req.query.id);
         if (state) {
             res.send(state)
